a5ce1e0f09a26dbcea8c1b4202e9502e2f73fb1f
w8r <alex@milevski.co>
2023-12-25 21:48:58 +0100

    Text rendering

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { Skia, Canvas, Circle, Group } from "@shopify/react-native-skia";
+import {
+  Skia,
+  Canvas,
+  Circle,
+  Group,
+  Text,
+  useFont,
+} from "@shopify/react-native-skia";
 import { GestureHandler } from "./GestureHandler";
 import { makeMutable } from "react-native-reanimated";
-import { Dimensions } from "react-native";
+import { Platform, Dimensions } from "react-native";
 import { getTransformFromShapes } from "./utils";
+
+// phyllotaxis
+// https://en.wikipedia.org/wiki/Phyllotaxis
+const phyllotaxis = (n: number) => {
+  const theta = Math.PI * (3 - Math.sqrt(5));
+  return (i: number) => {
+    const r = Math.sqrt(i / n) * 50;
+    const th = i * theta;
+    return { x: r * Math.cos(th), y: r * Math.sin(th), radius: 1 };
+  };
+};
+
+const getPoint = phyllotaxis(400);
+const points = Array.from({ length: 400 }, (_, i) => getPoint(i));
+const getRandomHexColor = () => {
+  const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+  return `#${hex.padStart(6, "0")}`;
+};
 
 // add zooming an panning
 
@@ -12,23 +37,32 @@ const Container = () => {
   const r = width * 0.13;
   const matrix = makeMutable(
     getTransformFromShapes(
-      [
-        { x: r, y: r, radius: r },
-        { x: width - r, y: r, radius: r },
-        { x: width / 2, y: width - r, radius: r },
-      ],
+      points,
+      // [
+      //   { x: r, y: r, radius: r },
+      //   { x: width - r, y: r, radius: r },
+      //   { x: width / 2, y: width - r, radius: r },
+      // ],
       width,
       height
     )
   );
+  const fontSize = 1;
+  const font = useFont(require("../../assets/SpaceMono-Regular.ttf"), fontSize);
 
   return (
     <GestureHandler matrix={matrix}>
       <Canvas style={{ width, height, overflow: "hidden" }}>
         <Group blendMode="multiply" matrix={matrix}>
-          <Circle cx={r} cy={r} r={r} color="cyan" />
+          {/* <Circle cx={r} cy={r} r={r} color="cyan" />
           <Circle cx={width - r} cy={r} r={r} color="magenta" />
-          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" /> */}
+          {points.map(({ x, y, radius }, i) => (
+            <Group key={i}>
+              <Circle cx={x} cy={y} r={radius} color={getRandomHexColor()} />
+              {/* <Text font={font} x={x} y={y + 1.5} color={"blue"} text="text" /> */}
+            </Group>
+          ))}
         </Group>
       </Canvas>
     </GestureHandler>
